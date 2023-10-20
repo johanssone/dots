b6790a556f4090878d21b23ca844b44d02575246
Erik Johansson <erik.johansson89@gmail.com>
2023-10-20 20:51:46 +0200

    updates

--- flake.nix
+++ flake.nix
@@ -1,12 +1,102 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs;
-  inputs.home-manager.url = github:nix-community/home-manager;
+  description = "My systems and homes";
+  
+  inputs = {
 
-  outputs = { self, nixpkgs, ... }@attrs: {
-    nixosConfigurations.hodgepodge = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = attrs;
-      modules = [ ./configuration.nix ];
+    # Principle inputs
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
+    nixos-hardware.url = "https://flakehub.com/f/NixOS/nixos-hardware/0.1.*.tar.gz";
+    home-manager = {
+      url = "https://flakehub.com/f/nix-community/home-manager/0.1.*.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+      url = "https://flakehub.com/f/nix-community/disko/1.*.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixos-flake.url = "github:srid/nixos-flake";
+    nix-darwin.url = "github:lnl7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    # secret mgmt
+    sops-nix.url = "https://flakehub.com/f/Mic92/sops-nix/0.1.*.tar.gz";
+
+    # Software
+    nixos-shell.url = "https://flakehub.com/f/Mic92/nixos-shell/1.*.tar.gz";
+    emanote.url = "github:srid/emanote";
+    nuenv.url = "https://flakehub.com/f/DeterminateSystems/nuenv/0.1.*.tar.gz";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+  
+  outputs = inputs@{ self, ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.nixos-flake.flakeModule
+        ./users
+        #./home
+        #./nixos
+        #./systems/hodgepodge
+      ];
+      flake = {
+        # Configurations for Linux (NixOS) systems
+        nixosConfigurations = {
+          hodgepodge = self.nixos-flake.lib.mkLinuxSystem {
+            imports = [
+              #self.nixosModules.default # Defined in nixos/default.nix
+              inputs.sops-nix.nixosModules.sops
+              ./systems/hodgepodge
+              #./nixos/server/harden.nix
+              #./nixos/docker.nix
+              #./nixos/lxd.nix
+              #./nixos/jenkins.nix
+            ];
+            services.tailscale.enable = true;
+            sops.defaultSopsFile = ./secrets.json;
+            sops.defaultSopsFormat = "json";
+          };
+        };
+     perSystem = { self', system, pkgs, lib, config, inputs', ... }: {
+        # NOTE: These overlays apply to the Nix shell only. See `nix.nix` for
+        # system overlays.
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.jenkins-nix-ci.overlay
+          ];
+        };
+
+        nixos-flake.primary-inputs = [ "nixpkgs" "home-manager" "nix-darwin" "nixos-flake" ];
+
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+          programs.nixpkgs-fmt.enable = true;
+          settings.formatter.nixpkgs-fmt.excludes =
+            let
+              nixosConfig = self.nixosConfigurations.hodgepodge;
+              jenkinsPluginsFile = nixosConfig.config.jenkins-nix-ci.plugins-file;
+            in
+            [ jenkinsPluginsFile ];
+        };
+
+        packages.default = self'.packages.activate;
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nixpkgs-fmt
+            pkgs.sops
+            pkgs.ssh-to-age
+            (
+              let nixosConfig = self.nixosConfigurations.hodgepodge;
+              in nixosConfig.config.jenkins-nix-ci.nix-prefetch-jenkins-plugins pkgs
+            )
+          ];
+        };
+        formatter = config.treefmt.build.wrapper;
+      };
     };
   };
 }
---
+++ users/config.nix
@@ -0,0 +1,12 @@
+{
+  myself = "ekan";
+  users = {
+    srid = {
+      name = "Erik Johansson";
+      email = "erik.johansson89@gmail.com";
+      sshKeys = [
+        ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMFoXIPXPIzvWwIbdzQb88jAH5MaNFIxaSAzNimjoxmA
+      ];
+    };
+  };
+}
